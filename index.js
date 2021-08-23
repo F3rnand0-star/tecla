3fce3211321e2277afb2a9c53ea3644b62fd1379
F3rnand0-star <ing.fernandoperez15@gmail.com>
2021-08-23 00:07:17 -0500

    Se crean funciones asincronas

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function name(params){
-    console.log("algo");
-}
+// function llamada(){
+//     console.log("Hola mundo");
+// }
+
+// function llamadora(call) {
+//     console.log("Dentro de esta funcion llamamos nuestro callback")
+//     call()
+// }
+
+// llamadora(llamada);
+//---------------------
+setTimeout(() => {
+    console.log('1.- Este texto va primero')
+}, 6000)
+console.log('2.- Este texto va segundo')
+setTimeout(() => {
+    console.log('3.- Este texto va tercero')
+}, 4000)
+console.log('4.- Este texto va cuarto')
+
+
+
+
